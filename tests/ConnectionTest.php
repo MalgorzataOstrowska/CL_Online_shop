4b8878be095441cfb4bbc02af2ed9e098efb384c
Malgorzata Ostrowska <ostrowska.malgorzata.ewa@gmail.com>
2017-02-11 22:35:10 +0100

    MySqlXML – version of the database for tests – cl_online_shop_Test.xml

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -13,14 +13,28 @@
  */
 class ConnectionTest  extends PHPUnit_Extensions_Database_TestCase
 {
-    protected function getConnection() { //26
+    protected function getConnection()
+    {
         $conn = new PDO(
                 $GLOBALS['DB_DNS'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWORD']
         );
         return new PHPUnit_Extensions_Database_DB_DefaultDatabaseConnection($conn, $GLOBALS['DB_DBNAME']);
     }
-    protected function getDataSet() { //26
-        return $this->createMySQLXMLDataSet('file.xml');
+    
+    protected function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet('cl_online_shop_Test.xml');
+        
+/*******************************************************************************
+creating a xml file in the terminal:
+    mysqldump --xml -t -u [username] -p [database]          > file.xml
+    mysqldump --xml    -u root       -p cl_online_shop_Test > cl_online_shop_Test.xml
+
+in phpunit.xml
+    <var name="DB_DNS" value="mysql:dbname=cl_online_shop_Test;host=localhost" />
+    ...
+    <var name="DB_DBNAME" value="cl_online_shop_Test" />
+*******************************************************************************/
     }
     
     public function testCosTam()
